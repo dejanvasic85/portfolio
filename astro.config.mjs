d909b2b7912cdde4e9f54b8e12b4d3e993b62462
Dejan Vasic <dejanvasic24@gmail.com>
2025-04-19 22:18:50 +1000

    Adding a simple portfolio website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      theme: "github-dark",
+      langs: [
+        "html",
+        "css",
+        "js",
+        "ts",
+        "jsx",
+        "tsx",
+        "json",
+        "markdown",
+        "python",
+        "bash",
+      ],
+      wrap: true,
+    },
+  },
+  integrations: [mdx()],
+});
