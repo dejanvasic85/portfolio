f86421d75811057b4c1fd8718c34fcbdb793aa72
Dejan Vasic <dejanvasic24@gmail.com>
2025-04-21 15:36:29 +1000

    Adding prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,15 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  markdown: {
-    shikiConfig: {
-      theme: "github-dark",
-      langs: [
-        "html",
-        "css",
-        "js",
-        "ts",
-        "jsx",
-        "tsx",
-        "json",
-        "markdown",
-        "python",
-        "bash",
-      ],
-      wrap: true,
-    },
-  },
-  integrations: [mdx()],
+	markdown: {
+		shikiConfig: {
+			theme: 'github-dark',
+			langs: ['html', 'css', 'js', 'ts', 'jsx', 'tsx', 'json', 'markdown', 'python', 'bash'],
+			wrap: true
+		}
+	},
+	integrations: [mdx()]
 });
